d098b8054f65293e322bb7dba306992cf2c3e0c4
Engin <yagmure15@itu.edu.tr>
2021-10-10 00:14:11 +0300

    Add project files.

---
+++ console-proj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test repo!");
+        }
+    }
+}
